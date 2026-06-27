2d9d8fb8a57bdf470211ce0197107161e87da3d2
hshuwaikh-lab <h.shuwaikh@gmail.com>
2026-06-27 03:23:48 +0300

    Fix: Convert server-static.js to ES module syntax

--- server-static.js
+++ server-static.js
@@ -1,9 +1,13 @@
 #!/usr/bin/env node
 
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
-const url = require('url');
+import http from 'http';
+import fs from 'fs';
+import path from 'path';
+import url from 'url';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const PORT = process.env.PORT || 3000;
 const DIST_DIR = path.join(__dirname, 'dist');
